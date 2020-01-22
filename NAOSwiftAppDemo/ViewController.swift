ac69baf78930dbe290631677d1c0b06a8abdf510
Honoré Bizagwira <honore.bizagwira@polestar.eu>
2020-01-22 12:44:18 +0100

    first commit

---
+++ NAOSwiftAppDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  NAOSwiftAppDemo
+//
+//  Created by Pole Star on 22/01/2020.
+//  Copyright © 2020 Pole Star. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var switchButton: UISwitch!
+    @IBOutlet weak var locationHandlerConsole: UITextView!
+    @IBOutlet weak var apikey: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
